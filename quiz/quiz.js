54f8f2e21574ccc3ed87b4257b70396fd6555020
lukassloth <lukas.sloth0@gmail.com>
2024-11-14 10:07:25 +0100

    modified

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,7 +1,30 @@
+function buildQuiz(){
+    const output = [];
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            const answers = [];
+            for(letter in currentQuestion.answers){
+                answers.push(
+                    `<label>
+                        <input type="radio" name="question${questionNumber}" value="${letter}">
+                        ${letter} : 
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div> 
+                <div class="answers"> ${answers.join('')} </div>`
+            );
+        }
+    );
+    quizContainer.innerHTML = output.join('');
+};
+
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
-
 const myQuestions = [
     {
         question: "spørgsmål 1?",
@@ -104,21 +127,3 @@ const myQuestions = [
         correctAnswer: "c"
     }
 ];
-
-function buildQuiz(){
-    const output = [];
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-            const answers = [];
-            for(letter in currentQuestion.answers){
-                answers.push(
-                    `<label>
-                        <input type="radio" name="question${questionNumber}" value="${letter}">
-                        ${letter} : 
-                        ${currentQuestion.answers[letter]}
-                    </label>`
-                );
-            }
-        }
-    )
-}
